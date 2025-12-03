069ea9543210ec33f17aaca02d095bf3928e0624
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2025-12-03 22:14:05 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -77,4 +77,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // CREATE INSTANCE
     new SignupForm();
+}
+    document.getElementById("signupBtn").addEventListener("click", function () {
+    // Redirect to signup page
+    window.location.href = "index.html";
 });
+);
