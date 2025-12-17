ac3b5689ea8a3549a936ac125e5c5df288aa72b2
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2025-12-17 19:06:39 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 // SIGN UP PAGE FUNCTIONALITY
 document.addEventListener("DOMContentLoaded", () => {
   const SERVER_URL =
-    "https://mybackend-production-b618.up.railway.app/submit";
+    "https://mybackend-production-b618.up.railway.app/signup";
 
   class SignupForm {
     constructor() {
