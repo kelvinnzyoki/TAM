b1b0cd39d8f715ad591758693f1255889a77f119
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2025-12-25 17:46:17 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -28,10 +28,10 @@ boxes.forEach(id => {
 
 // --- B. Handle "Other" Input Logic ---
 otherInput.addEventListener("input", () => {
-    const value = parseInt(otherInput.value);
+    const value = parseInt(otherInput.score);
 
     // If user starts typing, uncheck all boxes
-    if (otherInput.value !== "") {
+    if (!isNaN(score) && otherInput.score !== "") {
         boxes.forEach(id => document.getElementById(id).checked = false);
         recordBtn.disabled = false;
 
@@ -71,6 +71,7 @@ otherInput.addEventListener("input", () => {
         }
         else {
         recordBtn.disabled = true;
+    }
     }
 });
 
