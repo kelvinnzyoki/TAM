2564a7db57d7790611ab7db2b372b7cdf86e1530
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-01-30 01:54:02 +0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -29,7 +29,7 @@ recordBtn.addEventListener('click', async function() {
     };
 
     try {
-        const response = await fetch("https://mybackend-production-b618.up.railway.app/addictions", {
+        const response = await fetch("https://cctamcc.site/addictions", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(dataToRecord),
