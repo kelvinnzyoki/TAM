986d2c64e5751d22d30c342d42098cd41a32e4ca
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-01-30 08:30:52 +0300

    Update script6.js

--- script6.js
+++ script6.js
@@ -101,7 +101,7 @@ recordBtn.addEventListener("click", async () => {
 
         if (data.success) {
             alert("✅ Squats Recorded!");
-            window.location.replace = "/TAM/index2.html";
+            window.location.replace("/TAM/index2.html");
         } else {
             alert("Sync Failed: " + (data.message || "Unknown error"));
             recordBtn.disabled = false;
